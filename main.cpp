33881756e6339da2fe9615ba0ebef26d29caa866
Felix Bär <felix.baer.poltringen@gmx.de>
2025-04-17 14:25:33 +0200

    Board-Klasse und Board-node-struct angelegt

--- main.cpp
+++ main.cpp
@@ -8,50 +8,71 @@ using namespace std;
 
 class board
 {
-    int board_state[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0 = leer, 1 = X, -1=O
-    int turn;                                         // Anzahl Züge bis jetzt
-    bool next_player;                                 // 0 = O, 1 = X
-    board(int turn_nr, int currentstate[9], bool last_played)
-    {
-        for (int jj = 0; jj < 9; jj++)
+    int board_state[9]; // 0 = leer, 1 = X, -1=O
+    int turn;           // Anzahl Züge bis jetzt
+    bool next_player;   // 0 = O, 1 = X
+
+public:
+    board(int turn_nr, int currentstate[9], bool last_played) // Konstruktor, erzeugt ein neues Brett mit dem Status der in
+    {                                                         // currentstate übergeben wird, dem aktuellen Zug und welcher Spieler
+        for (int jj = 0; jj < 9; jj++)                        // als letztes gezogen hat
         {
             this->board_state[jj] = currentstate[jj];
         }
         this->turn = turn_nr;
         this->next_player = !last_played;
     }
-
-public:
-    int get_cell_state(int cell)
-    {
-        return this->board_state[cell];
+    board()                            // Überladener Konstruktor, erzeugt ein leeres Feld
+    {                                  // wenn keine Parameter übergeben werden.
+        for (int jj = 0; jj < 9; jj++) //
+        {                              //
+            this->board_state[jj] = 0; //
+        } //
+        this->turn = 0;        //
+        this->next_player = 1; // Standard: X fängt an
     }
-    int get_turn()
+    int get_cell_state(int cell)        // Gibt den Status einer bestimmten Zelle zurück,
+    {                                   // mit 0=leeres Feld,1 = X in diesem Feld,
+        return this->board_state[cell]; //-1 = O in diesem Feld
+    }
+    int get_turn() // Gibt den aktuellen Zug zurück (Standard getter-funktion)
     {
         return this->turn;
     }
-    int get_next_player()
+    int get_next_player() // Gibt zurück, welcher Spieler als nächstes Spielen darf
     {
         return this->next_player;
     }
-    int set_cell_state(int cell)
-    {
-        if (board_state[cell] != 0)
-        {
-            return -1;
+    int set_cell_state(int cell) // Setzt den Status einer Zelle in Abhängigkeit davon, wer
+    {                            // als nächster Spieler eingetragen ist. Gibt -1 zurück wenn das Feld belegt ist
+        if (cell > 8)            // und -2 wenn das Feld nicht existiert (Zahl zu groß/zu klein)
+        {                        //
+            return -2;           //
         }
-        if (next_player)
-        {
-            this->board_state[cell]++;
+        if (board_state[cell] != 0) // Überprüft ob das gewählte Feld leer ist
+        {                           //
+            return -1;              //
         }
-        else
-        {
-            this->board_state[cell]--;
+        if (next_player)               // Wenn der nächste Spieler X ist wird der Status von der gewählten Zelle
+        {                              // um 1 erhöht, das entspricht dann einem platzierten X in dieser Zelle
+            this->board_state[cell]++; //
         }
-        return 0;
+        else                           //
+        {                              // Andernfalls wird der Status um 1 reduziert, das entspricht
+            this->board_state[cell]--; // einem platzierten O in der Zelle
+        }
+        this->turn++; // Der Zug wird automatisch erhöht
+        return 0;     // Kein Fehler-code
     }
 };
 
+struct board_node
+{
+    board *this_board = new board();
+    int games_played = 0;
+    int games_won_by_player = 0;
+    vector<board *> next_boards;
+};
 void printfield()
 
 {
